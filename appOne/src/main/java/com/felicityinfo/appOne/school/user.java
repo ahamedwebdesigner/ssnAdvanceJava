93d613a98498200ffc643eb02f5096548a258200
ahamesye <ahamesye@AHAMESYE1.auth.hpicorp.net>
2018-03-24 17:06:35 +0530

    added school context
    1) added school-context-xml
    2) created user class

---
+++ appOne/src/main/java/com/felicityinfo/appOne/school/user.java
@@ -0,0 +1,47 @@
+package com.felicityinfo.appOne.school;
+
+public class user {
+	private String type;
+	private String Name;
+	private String studentClass;
+	
+	public user(String type, String name, String studentClass) {
+		super();
+		this.type = type;
+		Name = name;
+		this.studentClass = studentClass;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getStudentClass() {
+		return studentClass;
+	}
+
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+
+	@Override
+	public String toString() {
+		return "user [type=" + type + ", Name=" + Name + ", studentClass=" + studentClass + "]";
+	}
+	
+	
+	
+
+}
